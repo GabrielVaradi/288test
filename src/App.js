4a7518f99d3ba407f3552e975d8b00551d56def4
GabrielVaradi <gabriel.varadi.hallope.lecoq@gmail.com>
2019-11-13 22:24:10 -0500

    Add dropdowns

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Important from './Important.js';
-import Development from './Development.js';
+import Dropdowns from './Dropdowns.js';
 import './App.scss';
 
 class App extends Component {
---
+++ src/Dropdowns.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Accordion, AccordionItem } from 'react-sanfona';
+
+const Dropdowns = () => {
+
+    return (
+      <Accordion>
+        {[1, 2, 3, 4, 5].map(item => {
+          return (
+            <AccordionItem title={`Item ${item}`} expanded={item === 1}>
+              <div>
+                {`Item ${item} content`}
+              </div>
+            </AccordionItem>
+          );
+        })}
+      </Accordion>
+    );
+};
+export default Dropdowns;
+
+
+
+
