a9e66fc6544f69c74aa2a472d31999ee3b0e0334
GabrielVaradi <gabriel.varadi.hallope.lecoq@gmail.com>
2019-11-14 13:52:35 -0500

    Add dropdown menus

--- src/App.js
+++ src/App.js
@@ -6,7 +6,39 @@ import './App.scss';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      subjects: [
+        {
+          title: 'Development',
+          links: ['Learn Rockwood history', '20th Century', 'Notable People', 'Early History'],
+          cards: [{
+            header: 'Rockwood is situated around a series of roads',
+            link: 'Buy your souvenir',
+            image: 'https://www.iconsdb.com/icons/preview/pink/square-xxl.png'
+          }]
+        },
+        {
+          title: 'Briggs City',
+          links: ['Link 1', 'Link 2', 'Link 3']
+        },
+        {
+          title: 'Red-winged warbler',
+          links: ['Link 1', 'Link 2', 'Link 3']
+        },
+        {
+          title: `In the early 20th century, Rockwood's economy further diversified`,
+          links: ['Learn More']
+        },
+        {
+          title: 'Education',
+          links: ['Link 1', 'Link 2', 'Link 3']
+        },
+        {
+          title: 'The House of Rothschild',
+          links: ['Link 1', 'Link 2', 'Link 3']
+        },
+      ]
+    };
   }
 
   componentDidMount() {}
@@ -16,10 +48,12 @@ class App extends Component {
     return (
       <React.Fragment>
         <Important/>
-        <Development/>
+        <Dropdowns subjects={this.state.subjects}/>
       </React.Fragment>
     );
   }
 }
 
 export default App;
+
+
--- src/Dropdowns.js
+++ src/Dropdowns.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import { Accordion, AccordionItem } from 'react-sanfona';
 
-const Dropdowns = () => {
+
+const Dropdowns = ({ subjects }) => {
 
     return (
-      <Accordion>
-        {[1, 2, 3, 4, 5].map(item => {
+      <Accordion className='accordion' allowMultiple='true'>
+        {subjects.map(item => {
           return (
-            <AccordionItem title={`Item ${item}`} expanded={item === 1}>
-              <div>
-                {`Item ${item} content`}
-              </div>
+            <AccordionItem title={`${item.title}`} expanded={item === 1} className='accordionItem' expandedClassName='accordionExpanded' titleClassName='accordionTitle' bodyClassName='accordionBody'>
+                {item.links.map(link => {
+                  return (
+                  <div>{link}</div>)
+                })}
+                {item.cards &&
+                  <div> {item.title} </div>
+                }
+
             </AccordionItem>
           );
         })}
@@ -22,3 +28,8 @@ export default Dropdowns;
 
 
 
+//<i class="fa fa-sort-down"></i>
+
+// <div className="image">
+//                     <src={item.card.image} alt=""/>
+//                   </div>
